b3276cfd4721c9b41c28059faf8c8780a03e4b4d
KosToneT <koctya0007@yandex.ru>
2022-06-19 19:22:52 +0800

    Update Messages.java

--- VKAPI/Messages.java
+++ VKAPI/Messages.java
@@ -6,7 +6,7 @@ public class Messages {
     public static String getHistory(Requests requests, int offset, int count, String user_id){
         return "test";
     }
-    public void sendMessage(Requests requests,String user_id, int random_id, String message){
+    public static void sendMessage(Requests requests,String user_id, int random_id, String message){
         try{
            //message = URLEncoder.encode(message, "UTF-8");
            requests.createVKResponse("messages.send?"
@@ -17,5 +17,22 @@ public class Messages {
         }catch(Exception ex){
             System.out.println(ex);
         }
+    }
+    /*
+     * offset Смещение, необходимое для выборки определенного подмножества бесед.
+     * count Количество сообщений, которое необходимо получить (но не более 200).
+     * filter Типы бесед, которые нужно вернуть, перечисленные через запятую.
+Может принимать следующие значения:
+    •all — все беседы;
+    •important — беседы, отмеченные важными (доступно только в сообщениях сообществ);
+    •unanswered — беседы, отмеченные непрочтенными (доступно только в сообщениях сообществ);
+    •unread — беседы, в которых есть непрочтенные сообщения.
+    По умолчанию будут возвращены все беседы.
+     */
+    public static String getConversations(Requests requests, int offset, String filter){
+        return requests.createVKResponse(name+"getConversations?"
+                            + "offset="+offset
+                            + "&count="+count
+                            + "&filter="+filter);
     }
 }
